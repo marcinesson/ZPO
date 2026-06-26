9b744458736185fa8b23560b4981bc702a0f61c2
Jacob123 <qbastar@gmail.com>
2026-06-26 17:02:31 +0200

    stworzenie wypozyczalni aut.

---
+++ Wypozyczalnia_Samochodow/wypozyczalniaaut-projekt/src/test/java/com/wypozyczalniajakub/wypozyczalniaaut_projekt/WypozyczalniaautProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wypozyczalniajakub.wypozyczalniaaut_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypozyczalniaautProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
