623c387627d02b16939702a3555fcae45caf341f
Marcin Halczuk <marhalczuk@gmail.com>
2026-07-03 05:17:01 +0200

    Delete wypozyczalnia-samochodow directory

--- wypozyczalnia-samochodow/src/main/java/com/example/wypozyczalnia_samochodow/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.wypozyczalnia_samochodow.repository;
-
-import com.example.wypozyczalnia_samochodow.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
--- wypozyczalnia-samochodow/src/main/java/com/example/wypozyczalnia_samochodow/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.wypozyczalnia_samochodow.repository;
-
-import com.example.wypozyczalnia_samochodow.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    List<Reservation> findByUsername(String username);
-}
