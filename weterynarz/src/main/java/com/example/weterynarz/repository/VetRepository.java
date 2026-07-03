37313a6ce118dc1097cde7dfe108cb8474ac0e79
Marcin Halczuk <marhalczuk@gmail.com>
2026-07-03 05:16:52 +0200

    Delete weterynarz directory

--- weterynarz/src/main/java/com/example/weterynarz/repository/VetRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.weterynarz.repository;
-
-import com.example.weterynarz.model.Vet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VetRepository extends JpaRepository<Vet, Long> {}
